82030fc87b8db52786588a83be65b7c534037b2a
23120346 <quangsexy01@gmail.com>
2025-12-14 17:48:14 +0700

    basic dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import Header from "./components/Header";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+import Header from "./Header";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-background text-foreground">
       <Header />
       <NavBar />
-
-      <main className="w-full max-w-[1200px] mx-auto px-4 py-8">
-        {/* Nội dung chính của app */}
-      </main>
+      {/* content của bạn ở đây */}
       <Footer />
     </div>
   );
 }
-
-export default App;
